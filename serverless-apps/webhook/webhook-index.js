c51e2872cec319ea0a2c4078f1f8b1d03596a526
Štefan Malček <stefan.malcek1@gmail.com>
2020-08-22 19:36:27 +0200

    Create webhook-index.js

---
+++ serverless-apps/webhook/webhook-index.js
@@ -0,0 +1,30 @@
+var Crypto = require('crypto');
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    var hmac = Crypto.createHmac("sha1", "<default key>");
+    var signature = hmac.update(JSON.stringify(req.body)).digest('hex');
+    var shaSignature =  `sha1=${signature}`;
+    var gitHubSignature = req.headers['x-hub-signature'];
+
+    if (!shaSignature.localeCompare(gitHubSignature)) {
+        if (req.body.pages[0].title) {
+            context.res = {
+                body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
+            };
+        }
+        else {
+            context.res = {
+                status: 400,
+                body: ("Invalid payload for Wiki event")
+            }
+        }
+    }
+    else {
+        context.res = {
+            status: 401,
+            body: "Signatures don't match"
+        }; 
+    }
+};
