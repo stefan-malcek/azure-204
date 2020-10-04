0a9dc86bf72c2b635ad01c5237130ff26027aaeb
Stefan Malcek <stefan.malcek1@gmail.com>
2020-10-04 17:13:33 +0200

    Implement demo for creating blob container.

---
+++ store-data/PhotoSharingApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PhotoSharingApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+            var connectionString = configuration["StorageAccountConnectionString"];
+
+            if (!CloudStorageAccount.TryParse(connectionString, out CloudStorageAccount storageAccount))
+            {
+                Console.WriteLine("Unable to parse connection string");
+                return;
+            }
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var blobContainer = blobClient.GetContainerReference("photoblobs");
+            bool created = await blobContainer.CreateIfNotExistsAsync();
+
+            Console.WriteLine(created ? "Created the Blob container" : "Blob container already exists.");
+        }
+    }
+}
