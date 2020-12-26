9baa4b20cf3f905fe868ef9f8508cfd950e8983c
Štefan Malček <stefan.malcek1@gmail.com>
2020-12-26 10:47:05 +0100

    Embedding data.

---
+++ store-data/cosmos-sql/cosmos-sql/Orders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cosmos_sql
+{
+    class Orders
+    {
+        public int orderid { get; set; }
+        public int quantity { get; set; }
+
+        public Orders(int p_id,int p_quantity)
+        {
+            orderid = p_id;
+            quantity = p_quantity;
+        }
+    }
+}
--- store-data/cosmos-sql/cosmos-sql/Program.cs
+++ store-data/cosmos-sql/cosmos-sql/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Azure.Cosmos;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace cosmos_sql
 {
@@ -8,13 +10,14 @@ namespace cosmos_sql
     {
         static string database = "appdb";
         static string containername = "customer";
-        static string endpoint = "https://storageaccount01.documents.azure.com:443/";
+        static string endpoint = "https://appaccount4000.documents.azure.com:443/";
         static string accountkeys = "";
 
         static async Task Main(string[] args)
         {
             CreateNewItem().Wait();
             ReadItem().Wait();
+            ReadOrderItem().Wait();
             ReplaceItem().Wait();
             DeleteItem().Wait();
             Console.ReadLine();
@@ -32,6 +35,11 @@ namespace cosmos_sql
                 customer obj = new customer(1, "John", "Miami");
                 obj.id = Guid.NewGuid().ToString();
 
+                List<Orders> list_orders = new List<Orders>()
+                {
+                    new Orders(100,10), new Orders(101,20)
+                };
+                obj.order = list_orders;
 
                 ItemResponse<customer> response = await container_conn.CreateItemAsync(obj);
                 Console.WriteLine("Request charge is {0}", response.RequestCharge);
@@ -63,6 +71,32 @@ namespace cosmos_sql
                         Console.WriteLine("Customer name is {0}", obj.customername);
                         Console.WriteLine("Customer city is {0}", obj.city);
                     }
+                }
+
+            }
+        }
+
+        private static async Task ReadOrderItem()
+        {
+            using (CosmosClient cosmos_client = new CosmosClient(endpoint, accountkeys))
+            {
+
+                Database db_conn = cosmos_client.GetDatabase(database);
+
+                Container container_conn = db_conn.GetContainer(containername);
+                PartitionKey pk = new PartitionKey("Miami");
+                string id = "9b464759-42d3-4a10-b55a-e1e46ef487e0";
+
+                ItemResponse<customer> response = await container_conn.ReadItemAsync<customer>(id, pk);
+                customer customer_obj = response.Resource;
+
+                Console.WriteLine($"Customer Id is {customer_obj.customerid}");
+                Console.WriteLine($"Customer name is {customer_obj.customername}");
+                Console.WriteLine($"Customer city is {customer_obj.city}");
+                foreach (Orders obj in customer_obj.order)
+                {
+                    Console.WriteLine($"Order Id is {obj.orderid}");
+                    Console.WriteLine($"Order Quantity is {obj.quantity}");
                 }
 
             }
