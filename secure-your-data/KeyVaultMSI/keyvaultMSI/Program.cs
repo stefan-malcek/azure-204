ea48180f0f0bdcde0b8f50928228e6b9c5f767a4
Štefan Malček <stefan.malcek1@gmail.com>
2020-12-28 10:32:05 +0100

    Implement Managed Service principals.

---
+++ secure-your-data/KeyVaultMSI/keyvaultMSI/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keyvaultMSI
+{
+    class Program
+    {
+        static string keyvaultURI = "https://azurevault2020.vault.azure.net/secrets/demosecret";
+        
+
+        static void Main(string[] args)
+        {
+           GetSecret().GetAwaiter().GetResult();
+        }
+
+        static public async Task GetSecret()
+        {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+
+            var keyVaultClient = new KeyVaultClient(
+          new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            var secret = await keyVaultClient.GetSecretAsync(keyvaultURI)
+                .ConfigureAwait(false);
+
+            Console.WriteLine(secret.Value);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ secure-your-data/WebAppMSI/WebAppMSI/GetDetails.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace WebAppMSI
+{
+    public partial class GetDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            OnGetAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task OnGetAsync()
+        {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            var secret = await keyVaultClient.GetSecretAsync("https://demovault5001.vault.azure.net/secrets/demosecret")
+                    .ConfigureAwait(false);
+            Response.Write(secret.Value);
+        }
+    }
+}
