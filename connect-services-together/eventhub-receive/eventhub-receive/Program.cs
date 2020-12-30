2893f269b8c1024c97c9418841ed0fe738e46a4a
Štefan Malček <stefan.malcek1@gmail.com>
2020-12-30 15:38:54 +0100

    Add projects for event hubs.

---
+++ connect-services-together/eventhub-receive/eventhub-receive/Program.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eventhub_receive
+{
+    class Program
+    {
+        private static string connstring = "";
+        private static string hubname = "apphub";
+
+        static void Main(string[] args)
+        {
+            GetEvents().Wait();
+        }
+
+        static private async Task GetEvents()
+        {
+            EventHubConsumerClient client = new EventHubConsumerClient("$Default", connstring, hubname);
+
+            var cancellation = new CancellationToken();
+
+            Console.WriteLine("Getting the events");
+            await foreach (PartitionEvent Allevent in client.ReadEventsAsync(cancellation))
+            {
+                EventData event_data = Allevent.Data;
+                Console.WriteLine(Encoding.UTF8.GetString(event_data.Body.ToArray()));
+            }
+        }
+    }
+}
