81e457fd9326bfc1f324ca19cff9266c0b38f014
Štefan Malček <stefan.malcek1@gmail.com>
2020-12-30 11:54:54 +0100

    Add props.

--- connect-services-together/Service Bus - Topic Send/topic-send/Program.cs
+++ connect-services-together/Service Bus - Topic Send/topic-send/Program.cs
@@ -12,15 +12,18 @@ namespace topic_send
         private static string _bus_connectionstring = "";
         private static string _topic_name = "apptopic";
         private static ITopicClient _client;
+        private static string[] Exams = new string[] { "AZ-900", "AZ-300", "AZ-400", "AZ-500", "AZ-204" };
 
         static async Task Main(string[] args)
         {
             _client = new TopicClient(_bus_connectionstring, _topic_name);
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 5; i++)
             {
                 Order obj = new Order();
                 var _message = new Message(Encoding.UTF8.GetBytes(obj.ToString()));
+                _message.MessageId = i.ToString();
+                _message.UserProperties.Add("Category", Exams[i]);
 
                 Console.WriteLine($"Sending Message : {obj.Id} ");
                 await _client.SendAsync(_message);
