9be55bae791da3ca98dc58f1f58695f501165c58
Štefan Malček <stefan.malcek1@gmail.com>
2020-12-30 10:42:51 +0100

    Add projects for service bus.

---
+++ connect-services-together/Service Bus - Queue Send/queue-send/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace queue_send
+{
+    class Order
+    {
+        public string Id { get; set; }
+        public int quantity { get; set; }
+
+        public Order()
+        {
+            Id = Guid.NewGuid().ToString();
+            Random rnd = new Random();
+            quantity = rnd.Next(1000);
+        }
+
+        public override string ToString()
+        {
+            return $"Id : {Id}, Quantity : {quantity}";
+        }
+    }
+}
