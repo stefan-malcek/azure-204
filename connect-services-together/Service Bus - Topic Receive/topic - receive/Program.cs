f6e99be25f9639b6f5f3d25f2571b229feb5db6f
Štefan Malček <stefan.malcek1@gmail.com>
2020-12-30 11:47:02 +0100

    Add topic projects.

---
+++ connect-services-together/Service Bus - Topic Receive/topic - receive/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace topic___receive
+{
+    class Program
+    {
+        private static string _bus_connectionstring = "";
+
+        private static ISubscriptionClient _client;
+        private static string _subscription_name = "SubscriptionA";
+
+        static void Main(string[] args)
+        {
+            TopicFunction().GetAwaiter().GetResult();
+        }
+
+        static async Task TopicFunction()
+        {
+            ServiceBusConnectionStringBuilder builder = new ServiceBusConnectionStringBuilder(_bus_connectionstring);
+            _client = new SubscriptionClient(builder, _subscription_name);
+
+            var _options = new MessageHandlerOptions(ExceptionReceived)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            _client.RegisterMessageHandler(Process_Message, _options);
+            Console.ReadKey();
+        }
+
+
+        static async Task Process_Message(Message _message, CancellationToken _token)
+        {
+            Console.WriteLine(Encoding.UTF8.GetString(_message.Body));
+            await _client.CompleteAsync(_message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceived(ExceptionReceivedEventArgs args)
+        {
+            Console.WriteLine(args.Exception);
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ connect-services-together/Service Bus - Topic Send/topic-send/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using queue_send;
+
+namespace topic_send
+{
+    class Program
+    {
+        private static string _bus_connectionstring = "";
+        private static string _topic_name = "apptopic";
+        private static ITopicClient _client;
+
+        static async Task Main(string[] args)
+        {
+            _client = new TopicClient(_bus_connectionstring, _topic_name);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Order obj = new Order();
+                var _message = new Message(Encoding.UTF8.GetBytes(obj.ToString()));
+
+                Console.WriteLine($"Sending Message : {obj.Id} ");
+                await _client.SendAsync(_message);
+            }
+
+            Console.ReadKey();
+
+            await _client.CloseAsync();
+        }
+    }
+}
